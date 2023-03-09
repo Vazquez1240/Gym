74a58f804b43aefaae437f194bcf024facf86e5c
Martin <vazquezmartin1240@gmail.com>
2023-03-09 01:17:01 -0600

    se agregaron todas las funciones al archivo users de la carpeta de repository

--- api/app/repository/user.py
+++ api/app/repository/user.py
@@ -2,6 +2,13 @@ from sqlalchemy.orm import Session
 from app.db import models
 
 
+
+def get_users(db:Session):
+    data = db.query(models.User).all()
+    if (len(data) == 0):
+        return {"Vacio": "La base de datos esta vacia"}
+    return data
+
 def create_user(user,db:Session):
     useer = user.dict()
     new_user = models.User(
@@ -16,3 +23,28 @@ def create_user(user,db:Session):
     db.commit()
     db.refresh(new_user)
     return {"Exito":"Usuario creado con exito"}
+
+
+def get_user(user_id,db:Session):
+    useer = db.query(models.User).filter(models.User.id == user_id).first()
+    if (not useer):
+        return {"Error": "Usuario no encontrado"}
+    return useer
+
+
+def delete_user(user_id,db:Session):
+    useer = db.query(models.User).filter(models.User.id == user_id)
+    if (not useer.first()):
+        return {"Error": f"El usuario con el id: {user_id} no existe"}
+    useer.delete(synchronize_session=False)
+    db.commit()
+    return {"Exito":"Usuario eliminado correctamente"}
+
+
+def update_user(user_id,db:Session,updateUser):
+    useer = db.query(models.User).filter(models.User.id == user_id)
+    if (not useer.first()):
+        return {"Error": f"El usuario con el id: {user_id} no existe"}
+    useer.update(updateUser.dict(exclude_unset=True))
+    db.commit()
+    return {"Exito":"Usuario actualizado correctamente"}
--- api/app/routers/user.py
+++ api/app/routers/user.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter,Depends
 from app.schemas import User,ShowUser,UpdateUSer
 from app.db.database import get_db
 from sqlalchemy.orm import Session
-from app.db import models
 from typing import List
 from app.repository import user
 
@@ -14,38 +13,26 @@ router = APIRouter(
 
 @router.get("/",response_model=List[ShowUser])
 def obtener_usuarios(db:Session = Depends(get_db)):
-    data = db.query(models.User).all()
-    if(len(data) == 0):
-        return {"Vacio":"La base de datos esta vacia"}
-    return data
+    support = user.get_users(db)
+
+    return support
 @router.post("/create_user")
 def crear_usuario(useer:User,db:Session = Depends(get_db)):
     support = user.create_user(useer,db)
     return support
-@router.post("/{user_id}",response_model=ShowUser)
+@router.get("/{user_id}",response_model=ShowUser)
 def obtener_usuario(user_id:int,db:Session = Depends(get_db)):
-    useer = db.query(models.User).filter(models.User.id == user_id).first()
-    if(not useer):
-        return {"Error":"Usuario no encontrado"}
-    return useer
+    support = user.get_user(user_id,db)
+    return support
 
 
 @router.delete("/{user_id}")
 def delete_user(user_id:int,db:Session = Depends(get_db)):
-    useer = db.query(models.User).filter(models.User.id == user_id)
-    if (not useer.first()):
-        return {"Error":f"El usuario con el id: {user_id} no existe"}
-    useer.delete(synchronize_session=False)
-    db.commit()
-    return {"Exito":f"El usuario con el id: {user_id} Fue eliminado"}
+    support = user.delete_user(user_id,db)
+    return support
 
 
 @router.patch("/{user_id}")
 def update_user(user_id:int,updateUser:UpdateUSer,db:Session = Depends(get_db)):
-    useer = db.query(models.User).filter(models.User.id == user_id)
-    if (not useer.first()):
-        return {"Error":f"El usuario con el id: {user_id} no existe"}
-    useer.update(updateUser.dict(exclude_unset=True))
-    db.commit()
-
-    return {"Exito":f"El usuario con el id: {user_id} fue actualizado"}
+    support = user.update_user(user_id,db,updateUser)
+    return support
