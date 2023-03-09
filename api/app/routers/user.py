25d35b0f0016321f3c5ce92813c6d300000792aa
Martin <vazquezmartin1240@gmail.com>
2023-03-09 00:29:37 -0600

    Se agrego eliminar usuarios de la base de datos

--- api/app/routers/user.py
+++ api/app/routers/user.py
@@ -3,6 +3,7 @@ from app.schemas import User,UserId,ShowUser
 from app.db.database import get_db
 from sqlalchemy.orm import Session
 from app.db import models
+from typing import List
 
 router = APIRouter(
     prefix="/user",
@@ -11,12 +12,12 @@ router = APIRouter(
 
 usuarios = []
 
-@router.get("/")
+@router.get("/",response_model=List[ShowUser])
 def obtener_usuarios(db:Session = Depends(get_db)):
     data = db.query(models.User).all()
     if(len(data) == 0):
         return {"Vacio":"La base de datos esta vacia"}
-    return usuarios
+    return data
 @router.post("/create_user")
 def crear_usuario(user:User,db:Session = Depends(get_db)):
     useer = user.dict()
@@ -42,12 +43,13 @@ def obtener_usuario(user_id:int,db:Session = Depends(get_db)):
 
 
 @router.delete("/{user_id}")
-def delete_user(user_id:int):
-    for index,user in enumerate(usuarios):
-        if(user["id"] == user_id):
-            usuarios.pop(index)
-            return {"Exito":"Usuario eliminado correctamente"}
-    return {"Error":f"El usuario con el id: {user_id} no existe"}
+def delete_user(user_id:int,db:Session = Depends(get_db)):
+    useer = db.query(models.User).filter(models.User.id == user_id)
+    if (not useer.first()):
+        return {"Error":f"El usuario con el id: {user_id} no existe"}
+    useer.delete(synchronize_session=False)
+    db.commit()
+    return {"Exito":f"El usuario con el id: {user_id} Fue eliminado"}
 
 
 @router.put("/{user_id}")
