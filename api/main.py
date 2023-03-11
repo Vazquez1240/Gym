814c6c9b64b9fcca9de7993a02a4a49e8de03af1
Martin <vazquezmartin1240@gmail.com>
2023-03-10 21:25:32 -0600

    commit

--- api/main.py
+++ api/main.py
@@ -14,4 +14,4 @@ app.include_router(user.router)
 
 
 if(__name__ == '__main__'):
-    uvicorn.run("main:app",port=8000,reload=True,host='0.0.0.0')
+    uvicorn.run("main:app",port=8000,reload=True)
--- aplicacion web/main.py
+++ aplicacion web/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,redirect,url_for,render_template
+from flask import Flask, jsonify,redirect,url_for,render_template,request
 import requests
 
 
@@ -8,9 +8,29 @@ API_HOST = "http://localhost:8000/user"
 
 @app.route("/")
 def home():
-    response = requests.get(API_HOST+"/obtener_usuarios")
-    return response.json()
-
+    data = requests.get(API_HOST+"/obtener_usuarios")
+    return data.json()
+@app.route("/new_user",methods=["POST","GET"])
+def new_user():
+    if(request.method == "POST"):
+        name = request.form["name"]
+        surname = request.form["surname"]
+        username = request.form["username"]
+        password = request.form["password"]
+        number_phone = request.form["number_phone"]
+        mail = request.form["mail"]
+        user = {"name":name,"surname":surname,"username":username,"password":password,"number_phone":number_phone,
+                "mail":mail}
+        response = requests.post(API_HOST+'/create_user',json=user)
+        if(response.status_code == 201):
+            return "usuario creado correctamente"
+        else:
+            return "error"
+    elif(request.method == "GET"):
+        print("Entro al get")
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 if(__name__ == '__main__'):
     app.run(debug=True,port=5000,host='0.0.0.0')
